25d87bfbbaf9bd3104d33b00ea0a3149275c0a91
WNHS10220\morik <moriken060426@gmail.com>
2024-10-25 15:49:30 +0900

    ゲージの追加

--- Assets/moriken/Script/Gauge.cs
+++ Assets/moriken/Script/Gauge.cs
@@ -1,21 +1,40 @@
+using Unity.Properties;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Gauge : MonoBehaviour
 {
-    Image image;
-    [SerializeField] float maxPrameter;
-    [SerializeField] float prameter;
+    [SerializeField] private GameObject playerObject;
+
+    private Image image;
+    private Player player;
+
+    [SerializeField] private float maxParameter;
+    [SerializeField] private float parameter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         image = gameObject.GetComponent<Image>();
+
+
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        if (playerObject != null)
+        {
+            player = playerObject.GetComponent<Player>();
+            maxParameter = player.maxBoost;
+            image.fillAmount = maxParameter / 100;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        image.fillAmount = prameter / maxPrameter;
+        if (playerObject != null)
+        {
+            parameter = player.currentBoost;
+
+            image.fillAmount = parameter / 100;
+        }
     }
 }
