a1c855619ed886075a2eb68ff9a9096464526158
naoto12 <naoto_03_11m@yahoo.co.jp>
2024-11-08 13:12:48 +0900

    フェードの実装

--- Assets/Matsubara/Scripts/SceneTransitionManager.cs
+++ Assets/Matsubara/Scripts/SceneTransitionManager.cs
@@ -8,9 +8,15 @@ public class SceneTransitionManager : MonoBehaviour
     public static SceneTransitionManager instance;
     public GameObject loadingScreen;    // ロード画面のUI（プレハブなどをアタッチ）
     public Slider progressBar;          // ロード中の進捗を表示するスライダー   
+    //[SerializeField]
+    public GameObject fadePanel;        // フェードに使うパネル
+    // [SerializeField]
+    FadeManager fadeManager;
 
     [SerializeField]
-    private Vector3 playerStartPos = new Vector3(500.0f, 100.0f, 0.0f);
+    private Vector3 playerStartPos = new Vector3(0.0f, 0.0f, 0.0f);
+    [SerializeField]
+    private GameObject PlayerStartObj;
     private GameCheckPointManager gameCheckPointManager;
 
     private void Awake()
@@ -18,16 +24,27 @@ public class SceneTransitionManager : MonoBehaviour
         // シングルトンパターンの実装
         if (instance == null)
         {
+            if (!FadeManager.isFadeInstance)
+            {
+                Instantiate(fadePanel);
+                fadeManager = GameObject.FindWithTag("Fade").GetComponent<FadeManager>();
+
+            }
+
             instance = this;
             DontDestroyOnLoad(gameObject);  // シーン間でオブジェクトが破棄されないようにする
 
+            playerStartPos = PlayerStartObj.transform.position;
+
             // GameCheckPointManager取得
             gameCheckPointManager =  GameObject.Find("GameCheckPointManager").GetComponent<GameCheckPointManager>();
+            
         }
         else
         {
             Destroy(gameObject);
         }
+        
     }
 
     // シーンを即座に遷移させる（同期的に）
@@ -53,19 +70,32 @@ public class SceneTransitionManager : MonoBehaviour
         // ロード画面を表示
         loadingScreen.SetActive(true);
 
+        // フェードアウト開始
+        fadeManager.FadeOut();
+
         // シーンを非同期でロード
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-  
+        operation.allowSceneActivation = false;
+
+        yield return new WaitForSeconds(2);
+
         // ロード中の進捗を更新
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);
             progressBar.value = progress;  // 進捗バーの値を更新
+                                           // フェードイン開始
+            fadeManager.FadeIn();
+
+            operation.allowSceneActivation = true;  // シーン表示を許可
+
             yield return null;
         }
 
+ 
         // 次のシーンを格納
         gameCheckPointManager.ChangeStage(sceneName);
+
 
         // ロードが完了したらロード画面を非表示
         loadingScreen.SetActive(false);
