0c3981456866b75c577ad516dbc1df9f3317fd42
naoto12 <naoto_03_11m@yahoo.co.jp>
2024-12-19 00:02:10 +0900

    タイトル

--- Assets/Matsubara/Scripts/UITimer.cs
+++ Assets/Matsubara/Scripts/UITimer.cs
@@ -7,19 +7,25 @@ public class UITimer : MonoBehaviour
 
     private TextMeshProUGUI timerText;
 
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameManager = GameManager.instance;
 
         timerText = GetComponent<TextMeshProUGUI>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        timerText.text = gameManager.time.ToString("f2");
+        float time = gameManager.time; // •b”
+        int minutes = Mathf.FloorToInt(time / 60); // •ª‚ğŒvZ
+        int seconds = Mathf.FloorToInt(time % 60); // •b‚ğŒvZ
 
+        timerText.text = $"{minutes:00}:{seconds:00}";
     }
 
 
