e4bbfe9720f8bb0d194b49801b153530bfb08f37
WNHS10220\morik <moriken060426@gmail.com>
2025-01-10 18:19:38 +0900

    エリアCアイコン追加

--- Assets/moriken/Script/BillBoard.cs
+++ Assets/moriken/Script/BillBoard.cs
@@ -2,10 +2,18 @@ using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
+    public float nowPosi;
+    private void Start()
+    {
+        nowPosi = gameObject.transform.position.y;
+    }
+
     void Update()
     {
+        transform.position = new Vector3(transform.position.x, nowPosi + Mathf.Sin(3 * Time.time) / 5, transform.position.z);
+
         Vector3 p = Camera.main.transform.position;
-        p.y = transform.position.y;
-        transform.LookAt(p);
+        //p.y = transform.position.y;
+        transform.LookAt(p); 
     }
 }
